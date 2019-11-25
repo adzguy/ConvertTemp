99ce8441d62c6ca993e81c6932c30fb826b0a187
adzguy <dnurmeto@neiu.edu>
2019-11-25 16:01:16 -0600

    Temperature converting app is complete.

--- ConvertTemp/ContentView.swift
+++ ConvertTemp/ContentView.swift
@@ -9,8 +9,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var tempAmount = "0"
+    @State private var selectionFrom = 0
+    @State private var selectionTo = 1
+    let tempUnits = ["Celsius", "Fahrenheit", "Kelvin"]
+    
+    var displayUnitConverted: Double {
+
+        let doubleTempAmount = Double(tempAmount) ?? 0
+        let unitSelectedFrom = tempUnits[selectionFrom]
+        let unitSelectedTo = tempUnits[selectionTo]
+        var converted: Double
+
+        let fToC = (doubleTempAmount - 32) * 5/9
+        let fToK = (doubleTempAmount - 32) * 5/9 + 273.15
+        
+        let kToC = doubleTempAmount - 273.15
+        let kToF = (doubleTempAmount - 273.15) * 9/5 + 32
+        
+        let cToF = (doubleTempAmount * 9/5) + 32
+        let cToK = doubleTempAmount + 273.15
+        
+        if unitSelectedFrom == "Celsius" && unitSelectedTo == "Fahrenheit" {
+            converted = cToF
+        }
+        else if unitSelectedFrom == "Celsius" && unitSelectedTo == "Kelvin" {
+            converted = cToK
+        }
+        else if unitSelectedFrom == "Fahrenheit" && unitSelectedTo == "Celsius" {
+            converted = fToC
+        }
+        else if unitSelectedFrom == "Fahrenheit" && unitSelectedTo == "Kelvin" {
+            converted = fToK
+        }
+        else if unitSelectedFrom == "Kelvin" && unitSelectedTo == "Celsius" {
+            converted = kToC
+        }
+        else if unitSelectedFrom == "Kelvin" && unitSelectedTo == "Fahrenheit" {
+            converted = kToF
+        }
+        else {
+            converted = doubleTempAmount
+        }
+        
+        return converted
+
+    }
+    
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $tempAmount)
+                    Picker("Pick", selection: $selectionFrom) {
+                        ForEach(0..<tempUnits.count) {
+                            Text("\(self.tempUnits[$0])")
+                        }
+                    }
+                .pickerStyle(SegmentedPickerStyle())
+                }
+                .keyboardType(.numberPad)
+                Section(header: Text("Convert To: ")){
+                    Picker("Pick Unit", selection: $selectionTo) {
+                        ForEach(0..<tempUnits.count) {
+                            Text("\(self.tempUnits[$0])")
+                        }
+                    }
+                .pickerStyle(SegmentedPickerStyle())
+                }
+                Section {
+                    Text("\(displayUnitConverted, specifier: "%.2f")")
+                }
+            }
+            .navigationBarTitle("Temperature Convert", displayMode: .inline)
+        }
     }
 }
 
